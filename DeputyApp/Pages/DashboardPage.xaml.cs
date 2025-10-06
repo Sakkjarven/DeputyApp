a05b9958e4c73cb8823e4e3a39f64878fc7559e1
Egor <RLA@DESKTOP>
2025-10-06 15:38:53 +0500

    Dashboard add

---
+++ DeputyApp/Pages/DashboardPage.xaml.cs
@@ -0,0 +1,30 @@
+namespace DeputyApp.Pages
+{
+    public partial class DashboardPage : ContentPage
+    {
+        public DashboardPage()
+        {
+            InitializeComponent(); // <- ошибка? значит проблема с генерацией
+        }
+        private async void GoToSchedule(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//SchedulePage");
+        }
+
+        private async void GoToDocuments(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//DocumentsPage");
+        }
+
+        private async void GoToNotifications(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//NotificationsPage");
+        }
+
+        private async void Logout(object sender, EventArgs e)
+        {
+            // Пока просто возвращаем на страницу логина
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+    }
+}
---
+++ DeputyApp/Pages/DocumentPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace DeputyApp.Pages;
+
+public partial class DocumentPage : ContentPage
+{
+	public DocumentPage()
+	{
+		InitializeComponent();
+	}
+}
